6aef2e2f863e126189dc9e3687c87f8bda2e0ee7
ryu-tatsuno <fko2447011@stu.o-hara.ac.jp>
2025-05-07 11:26:02 +0900

    11

---
+++ src/Bean/Student.java
@@ -0,0 +1,63 @@
+package Bean;
+
+public class Student {
+    private String no;
+    private String name;
+    private int entYear;
+    private String classNum;
+    private boolean isAttend;
+    private String schoolCd;
+
+    // コンストラクタ（空のもの）
+    public Student() {
+    }
+
+    // ゲッターとセッター
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEntYear() {
+        return entYear;
+    }
+
+    public void setEntYear(int entYear) {
+        this.entYear = entYear;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    public boolean isAttend() {
+        return isAttend;
+    }
+
+    public void setAttend(boolean isAttend) {
+        this.isAttend = isAttend;
+    }
+
+    public String getSchoolCd() {
+        return schoolCd;
+    }
+
+    public void setSchoolCd(String schoolCd) {
+        this.schoolCd = schoolCd;
+    }
+}
