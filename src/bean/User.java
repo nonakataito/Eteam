de0be65a0384da36e702bf1f89ad90d733447298
t_nonaka <t_nonaka@LAPTOP-H1EUD4U7>
2025-05-13 10:48:18 +0900

    bean

---
+++ src/bean/User.java
@@ -0,0 +1,34 @@
+package bean;
+public class User {
+    private String username;
+    private String password;
+    private boolean isAuthenticated;
+
+    public User() {
+        this.isAuthenticated = false; // 初期状態では未認証
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAuthenticated() {
+        return isAuthenticated;
+    }
+
+    public void setAuthenticated(boolean isAuthenticated) {
+        this.isAuthenticated = isAuthenticated;
+    }
+}
