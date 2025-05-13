765332577e075cb950e553940555d2f6718e47de
t_nonaka <t_nonaka@LAPTOP-H1EUD4U7>
2025-05-13 11:00:08 +0900

    userbean変更

--- src/bean/User.java
+++ src/bean/User.java
@@ -1,33 +1,17 @@
 package bean;
 public class User {
-    private String username;
-    private String password;
     private boolean isAuthenticated;
 
     public User() {
-        this.isAuthenticated = false; // 初期状態では未認証
+        this.isAuthenticated = false; // 初期状態：未認証
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    // ゲッター（getではなくisを使うのがbooleanの慣習）
     public boolean isAuthenticated() {
         return isAuthenticated;
     }
 
+    // セッター
     public void setAuthenticated(boolean isAuthenticated) {
         this.isAuthenticated = isAuthenticated;
     }
