a4f0a7173f0315c71e489fc6a97eb55b7b64b89e
t_nonaka <t_nonaka@LAPTOP-H1EUD4U7>
2025-05-14 10:26:42 +0900

    小文字

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -23,7 +23,7 @@ public class TeacherDao extends Dao {
                 teacher.setName(rs.getString("name"));
 
                 School school = new School();
-                school.setCD(rs.getString("SCHOOL_CD"));
+                school.setCd(rs.getString("SCHOOL_CD"));
                 teacher.setSchool(school);
             }
         }
